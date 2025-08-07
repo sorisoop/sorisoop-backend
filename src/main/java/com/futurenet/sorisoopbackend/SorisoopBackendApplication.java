f27023da2350cb1a33080e15bf539754685ab660
hyeon8571 <w01085914442@gmail.com>
2025-08-07 11:40:48 +0900

    chore: 프로젝트 초기 셋팅

---
+++ src/main/java/com/futurenet/sorisoopbackend/SorisoopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.futurenet.sorisoopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorisoopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorisoopBackendApplication.class, args);
+    }
+
+}
