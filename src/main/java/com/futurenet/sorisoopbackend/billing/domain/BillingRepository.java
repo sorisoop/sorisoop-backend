2b9b51244a879a29775e04e8ecfc856b6c242290
sangzun-han <hsjun1996@naver.com>
2025-08-18 17:02:46 +0900

    feat: 카드 중복확인 쿼리 추가

--- src/main/java/com/futurenet/sorisoopbackend/billing/domain/BillingRepository.java
+++ src/main/java/com/futurenet/sorisoopbackend/billing/domain/BillingRepository.java
@@ -8,4 +8,12 @@ public interface BillingRepository {
     String getCustomerKeyByMemberId(@Param("memberId") Long memberId);
     void updateCustomerKey(@Param("memberId") Long memberId, @Param("customerKey") String customerKey);
     int hasActiveCard(@Param("memberId") Long memberId);
+    void deactivateCards(@Param("memberId") Long memberId);
+    void insertBillingCard(
+            @Param("memberId") Long memberId,
+            @Param("billingKey") String billingKey,
+            @Param("cardCompany") String cardCompany,
+            @Param("cardNumber") String cardNumber
+    );
+    int existsCard(@Param("memberId") Long memberId, @Param("issuerName") String issuerName, @Param("cardNumber") String cardNumber);
 }
