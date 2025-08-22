62307ed64ad153d52b08c978a6982b85e7f03361
sangzun-han <hsjun1996@naver.com>
2025-08-23 00:45:24 +0900

    refactor : Billing 패키지 제거

--- src/main/java/com/futurenet/sorisoopbackend/billing/application/BillingService.java
+++
@@ -1,9 +0,0 @@
-package com.futurenet.sorisoopbackend.billing.application;
-
-import com.futurenet.sorisoopbackend.billing.dto.response.CustomerKeyResponse;
-
-
-public interface BillingService {
-    CustomerKeyResponse getOrMakeCustomerKey(Long memberId);
-    void handleBrandpayAuth(Long memberId, String customerKey, String code);
-}
