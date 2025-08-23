21bf55fcd3cbaf547b2acb524a153e69b9955a35
seokyeong <aswe0409@naver.com>
2025-08-23 14:53:18 +0900

    feat: 카테고리별 동화책 조회

--- src/main/java/com/futurenet/sorisoopbackend/fairytale/application/FairyTaleService.java
+++ src/main/java/com/futurenet/sorisoopbackend/fairytale/application/FairyTaleService.java
@@ -1,12 +1,14 @@
 package com.futurenet.sorisoopbackend.fairytale.application;
 
 
-import com.futurenet.sorisoopbackend.fairytale.dto.response.FindFairyTaleContentResponse;
 import com.futurenet.sorisoopbackend.fairytale.dto.response.FairyTaleCategoryResponse;
+import com.futurenet.sorisoopbackend.fairytale.dto.response.FairyTaleResponse;
+import com.futurenet.sorisoopbackend.fairytale.dto.response.FindFairyTaleContentResponse;
 
 import java.util.List;
 
 public interface FairyTaleService {
     List<FindFairyTaleContentResponse> getFairyTaleContents(Long fairyTaleId);
     List<FairyTaleCategoryResponse> getFairyTaleCategories();
+    List<FairyTaleResponse> getFairyTaleList(Long categoryId, int page);
 }
