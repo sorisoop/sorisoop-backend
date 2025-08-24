8460832f5d49d4ef75cc076b9c50233b5887a34c
seokyeong <aswe0409@naver.com>
2025-08-24 15:04:19 +0900

    feat: 찜 동화책 조회, 수정 기능 구현

--- src/main/java/com/futurenet/sorisoopbackend/fairytale/application/FavoriteService.java
+++ src/main/java/com/futurenet/sorisoopbackend/fairytale/application/FavoriteService.java
@@ -1,5 +1,11 @@
 package com.futurenet.sorisoopbackend.fairytale.application;
 
+import com.futurenet.sorisoopbackend.fairytale.dto.response.FairyTaleResponse;
+
+import java.util.List;
+
 public interface FavoriteService {
     void addFavorite(Long fairyTaleId, Long profileId);
+    List<FairyTaleResponse> getFavoriteFairyTales(Long profileId, int page);
+    void deleteFavorite(Long fairyTaleId, Long profileId);
 }
--- src/main/java/com/futurenet/sorisoopbackend/fairytale/domain/FavoriteRepository.java
+++ src/main/java/com/futurenet/sorisoopbackend/fairytale/domain/FavoriteRepository.java
@@ -1,9 +1,14 @@
 package com.futurenet.sorisoopbackend.fairytale.domain;
 
+import com.futurenet.sorisoopbackend.fairytale.dto.response.FairyTaleResponse;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface FavoriteRepository {
-    int getFavoriteCount(Long fairyTaleId, Long profileId);
+    boolean getFavoriteCount(Long fairyTaleId, Long profileId);
     int addFavorite(Long fairyTaleId, Long profileId);
+    List<FairyTaleResponse> getFavoriteFairyTalesByProfileId(Long profileId, int start, int size);
+    int deleteFavorite(Long fairyTaleId, Long profileId);
 }
