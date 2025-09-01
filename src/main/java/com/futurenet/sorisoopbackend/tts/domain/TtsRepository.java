2c3f086b1a33fc4b70af7ea747d21e29942a6d5f
seokyeong <aswe0409@naver.com>
2025-09-01 12:07:35 +0900

    teat: tts

--- src/main/java/com/futurenet/sorisoopbackend/tts/domain/TtsRepository.java
+++ src/main/java/com/futurenet/sorisoopbackend/tts/domain/TtsRepository.java
@@ -1,8 +1,12 @@
 package com.futurenet.sorisoopbackend.tts.domain;
 
+import com.futurenet.sorisoopbackend.tts.dto.TtsDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface TtsRepository {
     String getVoiceUrlById(Long voiceId);
+    List<TtsDto> getFairyTaleList(Long fairyTaleId);
 }
---
+++ src/main/java/com/futurenet/sorisoopbackend/tts/dto/TtsDto.java
@@ -0,0 +1,15 @@
+package com.futurenet.sorisoopbackend.tts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TtsDto {
+    private int page;
+    private String script;
+}
