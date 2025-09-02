ab0e8978b4126c5920861b6cc1e6c8982c49f5b5
seokyeong <aswe0409@naver.com>
2025-09-02 11:51:33 +0900

    feat: 커스텀 동화 읽어주기

--- src/main/java/com/futurenet/sorisoopbackend/tts/application/TtsService.java
+++ src/main/java/com/futurenet/sorisoopbackend/tts/application/TtsService.java
@@ -1,5 +1,6 @@
 package com.futurenet.sorisoopbackend.tts.application;
 
+import com.futurenet.sorisoopbackend.tts.dto.request.GetCustomTtsRequest;
 import com.futurenet.sorisoopbackend.tts.dto.request.GetTtsRequest;
 import com.futurenet.sorisoopbackend.tts.dto.response.GetTtsResponse;
 import com.futurenet.sorisoopbackend.tts.dto.response.GetVoiceUuidResponse;
@@ -8,4 +9,5 @@ public interface TtsService {
     GetVoiceUuidResponse addSpeakers(Long voiceId);
     GetTtsResponse createTts(GetTtsRequest request);
     GetTtsResponse getTts(String voiceUuid, int page);
+    GetTtsResponse createCustomTts(GetCustomTtsRequest request);
 }
